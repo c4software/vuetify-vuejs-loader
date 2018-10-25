4ee48dac366c8b5d30b75da4e039ddc4dc5cf619
Valentin Brosseau <c4software@gmail.com>
2018-10-25 23:07:11 +0200

    Update name

---
+++ dist/vuetify-vuejs-loader.es.js
@@ -0,0 +1,78 @@
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+var script = {
+  name: 'VuetifyLoader'
+};
+
+/* script */
+            const __vue_script__ = script;
+            
+/* template */
+var __vue_render__ = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('v-dialog',{attrs:{"value":"true","persistent":"","max-width":"290"}},[_c('v-card',[_c('v-card-text',{staticClass:"text-xs-center"},[_c('v-progress-circular',{staticClass:"primary--text",attrs:{"size":70,"indeterminate":""}})],1)],1)],1)};
+var __vue_staticRenderFns__ = [];
+
+  /* style */
+  const __vue_inject_styles__ = undefined;
+  /* scoped */
+  const __vue_scope_id__ = undefined;
+  /* module identifier */
+  const __vue_module_identifier__ = undefined;
+  /* functional template */
+  const __vue_is_functional_template__ = false;
+  /* component normalizer */
+  function __vue_normalize__(
+    template, style, script$$1,
+    scope, functional, moduleIdentifier,
+    createInjector, createInjectorSSR
+  ) {
+    const component = (typeof script$$1 === 'function' ? script$$1.options : script$$1) || {};
+
+    // For security concerns, we use only base name in production mode.
+    component.__file = "VuetifyLoader.vue";
+
+    if (!component.render) {
+      component.render = template.render;
+      component.staticRenderFns = template.staticRenderFns;
+      component._compiled = true;
+
+      if (functional) component.functional = true;
+    }
+
+    component._scopeId = scope;
+
+    return component
+  }
+  /* style inject */
+  
+  /* style inject SSR */
+  
+
+  
+  var VuetifyLoader = __vue_normalize__(
+    { render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ },
+    __vue_inject_styles__,
+    __vue_script__,
+    __vue_scope_id__,
+    __vue_is_functional_template__,
+    __vue_module_identifier__,
+    undefined,
+    undefined
+  )
+
+function Install(Vue) {
+  Vue.component('VuetifyLoader', VuetifyLoader);
+}
+
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(Install);
+}
+
+export default Install;
